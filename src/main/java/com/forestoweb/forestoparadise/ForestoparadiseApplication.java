0810c3b45e5f6650a48c63ff763baa60cb888b01
adarshrishu08 <adarshrishu08@gmail.com>
2018-03-07 18:43:30 +0530

    Foresto App First Commit Base Code.

---
+++ src/main/java/com/forestoweb/forestoparadise/ForestoparadiseApplication.java
@@ -0,0 +1,12 @@
+package com.forestoweb.forestoparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestoparadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestoparadiseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/forestoweb/forestoparadise/ForestoparadiseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.forestoweb.forestoparadise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForestoparadiseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
