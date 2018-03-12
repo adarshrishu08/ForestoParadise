8c4f2c88204d6cc58ccd334f0d22034e5fc6946d
Adarsh Rishu <adarshrishu08@gmail.com>
2018-03-12 10:14:53 +0530

    Foresto : Base Project

---
+++ foresto/src/main/java/com/webapp/foresto/ForestoApplication.java
@@ -0,0 +1,12 @@
+package com.webapp.foresto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestoApplication.class, args);
+	}
+}
